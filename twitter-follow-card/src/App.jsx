3b9aaccfa2ed3fecb4dbc2d5673fe2e5768996c5
Remy349 <santoraga15@gmail.com>
2023-07-19 00:36:35 -0600

    Added some styles and create the component.

--- twitter-follow-card/src/App.jsx
+++ twitter-follow-card/src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 
 function App() {
-  return <h1>Twitter follow card</h1>
+  return (
+    <section className='App'>
+      <TwitterFollowCard
+        username='remy349'
+        name='Santiago Moraga Caldera'
+        isFollowing={true}
+      />
+      <TwitterFollowCard username='fazt' name='Fazt' isFollowing={false} />
+      <TwitterFollowCard
+        username='midudev'
+        name='Miguel Ángel Durán'
+        isFollowing={true}
+      />
+      <TwitterFollowCard
+        username='twitter'
+        name='Twitter'
+        isFollowing={false}
+      />
+    </section>
+  )
 }
 
 export default App
