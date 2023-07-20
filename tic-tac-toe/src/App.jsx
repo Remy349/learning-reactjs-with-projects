60e3066df6c1bd76a27dec2fc5709ec93b8b48ca
Remy349 <santoraga15@gmail.com>
2023-07-19 19:30:07 -0600

    Added localStorage support for save the game.

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -5,16 +5,26 @@ import { TURNS } from './constants'
 import { checkWinnerFrom, checkEndGame } from './logic/board'
 import { Square } from './components/Square'
 import { WinnerModal } from './components/WinnerModal'
+import { saveGameStorage, resetGameStorage } from './logic/storage'
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
   const [winner, setWinner] = useState(null)
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()
   }
 
   const updateBoard = (index) => {
@@ -26,6 +36,8 @@ function App() {
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    saveGameStorage({ board: newBoard, turn: newTurn })
 
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
