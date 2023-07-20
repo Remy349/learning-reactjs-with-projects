1f65f4038ec1d9d70458b2d3a7b5e2702e41f548
Remy349 <santoraga15@gmail.com>
2023-07-20 15:28:15 -0600

    Agregue estilos basicos y separe separe el useEffect.
    El useEffect hacia dos fetch de las APIs, asi que lo hice dos useEffect para que cada uno se encargue de un fetch.

--- reactjs-prueba-tecnica/src/App.jsx
+++ reactjs-prueba-tecnica/src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?json=true`
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export function App() {
-  const [fact, setFact] = useState('')
-  const [imgUrl, setImgUrl] = useState('')
+  const [fact, setFact] = useState()
+  const [imgUrl, setImgUrl] = useState()
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
@@ -14,17 +14,21 @@ export function App() {
       .then((data) => {
         const { fact } = data
         setFact(fact)
-
-        const firstWord = fact.split(' ')[0]
-
-        fetch(`https://cataas.com/cat/says/${firstWord}?json=true`)
-          .then((res) => res.json())
-          .then((data) => {
-            const { url } = data
-            setImgUrl(url)
-          })
       })
   }, [])
+
+  useEffect(() => {
+    if (!fact) return
+
+    const firstWord = fact.split(' ')[0]
+
+    fetch(`https://cataas.com/cat/says/${firstWord}?json=true`)
+      .then((res) => res.json())
+      .then((data) => {
+        const { url } = data
+        setImgUrl(url)
+      })
+  }, [fact])
 
   return (
     <main>
