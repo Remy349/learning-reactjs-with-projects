16113837b8419dab401044bcaf0f14e4b4630f24
Remy349 <santoraga15@gmail.com>
2023-07-20 15:03:36 -0600

    Prueba finalizada.

---
+++ reactjs-prueba-tecnica/src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?json=true`
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+
+export function App() {
+  const [fact, setFact] = useState('')
+  const [imgUrl, setImgUrl] = useState('')
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((res) => res.json())
+      .then((data) => {
+        const { fact } = data
+        setFact(fact)
+
+        const firstWord = fact.split(' ')[0]
+
+        fetch(`https://cataas.com/cat/says/${firstWord}?json=true`)
+          .then((res) => res.json())
+          .then((data) => {
+            const { url } = data
+            setImgUrl(url)
+          })
+      })
+  }, [])
+
+  return (
+    <main>
+      <h1>ReactJS prueba tecnica - App de gatitos</h1>
+      {fact && <p>{fact}</p>}
+      {imgUrl && (
+        <img
+          src={`${CAT_PREFIX_IMAGE_URL}${imgUrl}`}
+          alt={`Image extracted using the first word for ${fact}`}
+        />
+      )}
+    </main>
+  )
+}
